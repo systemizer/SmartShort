c27e447857e0170190d9c3275e49b305cadac094
Rob McQueen <robmcqn@gmail.com>
2012-02-18 21:58:51 -0500

    removing uneccessary files. adding simple splash page. adding media link

--- local_settings.py
+++
@@ -1,6 +0,0 @@
-STATIC_ROOT = '/Users/rob/Documents/myprojects/djangoprojects/smartshort/static'
-MEDIA_ROOT = '/Users/rob/Documents/myprojects/djangoprojects/smartshort/mystatic'
-
-STATICFILES_DIRS = (
-    '/Users/rob/Documents/myprojects/djangoprojects/smartshort/mystatic',
-)
